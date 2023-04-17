28b3ca4bcd696946a434c64f0559eed1b4215531
HagbardCeline <janpauldahlke@gmail.com>
2023-04-17 12:00:57 +0200

    hello world route on axum

---
+++ Readme.md
@@ -0,0 +1,5 @@
+## axum - another rust webframework
+
+#### an exercise
+
+* to run  `cargo watch -x check -x run` (assumes you have cargo watch crate installed)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused)]
+
+use std::net::SocketAddr;
+
+use axum::{response::Html, routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let routes_hello = Router::new().route(
+        "/hello",
+        get(|| async { Html("Hello <strong> World!!! </strong>") }),
+    );
+
+    // region : Server
+    //which adress using std
+    let address = SocketAddr::from(([127, 0, 0, 1], 8000));
+    println!("--> LISTENING on {address}\n");
+
+    //bind the address to the server
+    axum::Server::bind(&address)
+        .serve(routes_hello.into_make_service()) //note the .into_make_service() #https://docs.rs/axum/latest/axum/struct.Router.html#method.into_make_service
+        .await
+        .unwrap();
+    // endregion : Server
 }
